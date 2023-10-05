ed15f6903b247778d437303166223d86ab406baa
MartinFortuna <martinperricault@gmail.com>
2023-10-05 22:54:07 +0000

    Added edit profile feature

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from profiles import views as profileViews
 
 urlpatterns = [
-    path('', profileViews.user_profile, name='user_profile')
+    path('', profileViews.user_profile, name='user_profile'),
+    path('update_profile/', profileViews.update_profile, name='update_profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include, reverse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from profiles.models import UserDetail
+from profiles.forms import UserForm, UserProfileForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -18,4 +21,34 @@ def user_profile(request):
     }
 
     template = 'profiles/user_profile.html'
+    return render(request, template, context)
+
+
+@login_required
+def update_profile(request):
+    """ Updates the profile info"""
+    user = request.user
+    profile, created = UserDetail.objects.get_or_create(user_id=user)
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=user)
+        profile_form = UserProfileForm(request.POST, instance=profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was updated!')
+            return HttpResponseRedirect(reverse('user_profile'))
+        else:
+            messages.error(request, 'Your profile was not updated, check the form')
+    else:
+        user_form = UserForm(instance=user)
+        profile_form = UserProfileForm(instance=profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+
+    template = 'profiles/update_profile.html'
     return render(request, template, context)
