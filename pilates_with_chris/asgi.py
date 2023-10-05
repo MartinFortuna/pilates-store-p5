9327d5705042dcbc8f6607bf553c2bb8c08a3844
MartinFortuna <martinperricault@gmail.com>
2023-10-05 10:17:19 +0000

    Initial commit

---
+++ pilates_with_chris/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pilates_with_chris project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pilates_with_chris.settings')
+
+application = get_asgi_application()
