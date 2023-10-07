5cbbb2c658fabbbac082b98eff8fb3d32db1215a
MartinFortuna <martinperricault@gmail.com>
2023-10-07 23:09:03 +0000

    Added custom 404 page

--- pilates_with_chris/urls.py
+++ pilates_with_chris/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
 ]
+
+handler404 = 'pilates_with_chris.views.handler404'
